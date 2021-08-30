ed936a67e1f67cf390dbdb697df375f1d5f7e5bd
arr331 <adrianrr1331@gmail.com>
2021-08-30 00:21:13 -0500

    Estilo en login

--- src/app/pages/site/site.component.ts
+++ src/app/pages/site/site.component.ts
@@ -66,7 +66,7 @@ export class SiteComponent implements OnInit {
       image: [''],
       x: ['', Validators.required],
       y: ['', Validators.required],
-      reference: [''],
+      reference: ['', Validators.required],
       state: [true],
     });
   }
